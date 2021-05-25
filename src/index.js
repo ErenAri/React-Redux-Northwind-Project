bb46ca269994ca0950e660fe070fff5595475cc8
Eren Arı <erenari27@gmail.com>
2021-05-25 19:21:01 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/root/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.minc.css'
+import { Provider } from 'react-redux';
+import configureStore from "./redux/reducers/configureStore"
 
+const store = configureStore()
 ReactDOM.render(
-  <React.StrictMode>
-    <App /> 
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App></App>
+  </Provider> 
+  
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,8 @@
+import{combineReducers} from "redux"
+import changeCategoryReducer from "./changeCategoryReducers"
+
+const rootReducer = combineReducers({
+    changeCategoryReducer
+})
+
+export default rootReducer;
